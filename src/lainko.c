0bfaa41efc72c2c0208b692156af4116b18d652b
vykt <knikanchyk@icloud.com>
2024-08-24 23:02:39 +0100

    fix compile-time errors

--- src/lainko.c
+++ src/lainko.c
@@ -1,13 +1,3 @@
-/*
- *  TODO TODO TODO TODO
- *
- *  1) Make userspace determine themselves how much extra space they want to add
- *     to the buffer, AND get rid of using count in the while loop, its pointless 
- *     if userspace determines the size.
- *
- *  2) Make get_map_sz return the number of VMAs, not the size of them in bytes.
- */
-
 #include <linux/kernel.h>
 #include <linux/types.h>
 
@@ -15,6 +5,7 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/kobject.h>
+#include <linux/device/class.h>
 
 #include <linux/fs.h>
 #include <linux/sysfs.h>
@@ -25,6 +16,7 @@
 #include <linux/sched.h>
 #include <linux/sched/task.h>
 #include <linux/sched/mm.h>
+#include <linux/mm.h>
 #include <linux/mm_types.h>
 #include <linux/mempolicy.h>
 #include <linux/ptrace.h>
@@ -37,7 +29,8 @@
 
 #include <linux/minmax.h>
 
-#include <asm-generic/page.h>
+//#include <asm-generic/page.h>
+#include <asm/page.h>
 
 #include <uapi/asm-generic/ioctl.h>
 #include <uapi/asm-generic/errno.h>
@@ -52,20 +45,17 @@ MODULE_AUTHOR("vykt");
 MODULE_DESCRIPTION("Kernel interface for foreign process memory manipulation.");
 MODULE_VERSION("1.0");
 
-
-
 #define MEMU_DEVICE_NAME "lainmemu"   //device will appear as /dev/lainmemu
 #define CLASS_NAME "lainko"           //class will appear as  /sys/class/lainko
 
+#define MEMU_MAP_SZ_XTRA_PCNT 10      //determines extra % in map buf (see design.txt)
 
-typedef char lain_byte;
-typedef unsigned char lain_ubyte;
 
 
 //globals
-static char dev_major;
+static char lainmemu_major;
 static struct device * lainmemu_device = NULL;
-static struct class * lain_class = NULL;
+static struct class * lainko_class = NULL;
 
 //ioctl call numbers
 static int lainmemu_ioctl_open_tgt;
@@ -92,31 +82,19 @@ static struct file_operations file_ops = {
     .unlocked_ioctl = memu_ioctl,
     .read = memu_read,
     .write = memu_write
-}
-
-
-
-//attribute related structures
-static struct kobj_attribute memu_major_attr = {0};
-
-
-static struct attribute_group memu_attrs_arr = {
-    &memu_major_attr,
-    NULL
 };
 
 
-static struct attribute_group memu_attr_group = {
-    .name = NULL,
-    .attrs = memu_attrs_arr,
-    .bin_attrs = NULL,
-    .is_visible = NULL,
-    .is_bin_visible = NULL
-}
+
+//sysfs attributes
+static ssize_t lainmemu_major_show(struct device * dev, 
+                               struct device_attribute * attr, char * buf);
+
+static struct device_attribute lainmemu_major_attr = __ATTR_RO(lainmemu_major);
 
 
 //local memu data
-struct local_data = {
+struct local_data {
 
     struct task_struct * target_task;
     struct mm_struct * target_mm;
@@ -125,58 +103,66 @@ struct local_data = {
     struct vm_entry * vm_map;
     int vm_map_sz; //in # of vm_entry
 
-    lain_byte * tx_buf;
+    lainko_byte * tx_buf;
     ssize_t tx_buf_sz; //in bytes
-}
+};
 
+
+
+// --- SYSFS ---
+static ssize_t lainmemu_major_show(struct device * dev, 
+                                   struct device_attribute * attr, char * buf) {
+
+    return sprintf(buf, "%d\n", lainmemu_major);
+}
 
 
 // --- INIT ---
 
 //get a dynamic major number
-static long register_major() {
+static long memu_register_major(void) {
  
     //errno technically goes up to 133 as of today, but we won't receive an errno > 127
-    dev_major = (char) register_chrdev(0, MEMU_DEVICE_NAME, &file_ops);
-    if (unlikely(dev_major < 0)) {
+    lainmemu_major = (char) register_chrdev(0, MEMU_DEVICE_NAME, &file_ops);
+    if (unlikely(lainmemu_major < 0)) {
         
         printk(KERN_ALERT 
           "[lainmemu][ERR] failed to register with a dynamic major device number.\n");
-        return (long) dev_major;
+        return (long) lainmemu_major;
     }
 
     printk(KERN_INFO 
-      "[lainmemu][OK] registered with major device number %d.\n", dev_major);
+      "[lainmemu][OK] registered with major device number %d.\n", lainmemu_major);
 
     return 0;
 }
 
-//create new lain class
-static long register_class() {
+//create new lainko class
+static long register_class(void) {
 
-    lain_class = class_create(THIS_MODULE, CLASS_NAME);
-    if (unlikely(IS_ERR(lain_class))) {
+    lainko_class = class_create(CLASS_NAME);
+    if (unlikely(IS_ERR(lainko_class))) {
         
-        unregister_chrdev(dev_major, MEMU_DEVICE_NAME);
+        unregister_chrdev(lainmemu_major, MEMU_DEVICE_NAME);
         printk(KERN_ALERT "[lainmemu][ERR] failed to register a device class.\n");
-        return PTR_ERR(lain_class);
+        return PTR_ERR(lainko_class);
     }
 
-    printk(KERN_INFO "[lainmemu][OK] created 'lain' class in sysfs.\n");
+    printk(KERN_INFO "[lainmemu][OK] created 'lainko' class in sysfs.\n");
 
     return 0;
 }
 
 
 //create a new lainmemu device
-static long register_device() {
+static long memu_register_device(void) {
 
-    lain_device =
-        device_create(lain_class, NULL, MKDEV(dev_major, 0), NULL, MEMU_DEVICE_NAME); 
-    if (unlikely(IS_ERR(lain_device))) {
+    lainmemu_device =
+        device_create(lainko_class, NULL, MKDEV(lainmemu_major, 0), NULL, MEMU_DEVICE_NAME); 
+    if (unlikely(IS_ERR(lainmemu_device))) {
         
-        class_destroy(lain_class);
-        unregister_chrdev(dev_major, MEMU_DEVICE_NAME);
+        class_destroy(lainko_class);
+        unregister_chrdev(lainmemu_major, MEMU_DEVICE_NAME);
         printk(KERN_ALERT "[lainmemu][ERR] failed to create a device.\n");
         return PTR_ERR(lainmemu_device);   
     }
@@ -187,55 +173,54 @@ static long register_device() {
 }
 
 
-//fill lainmemu ioctl numbers
-static long fill_lainmemu_ioctl_nums() {
-
-    lainmemu_ioctl_open_tgt    = _IOR((char) dev_major, LAINMEMU_IOCTL_OPEN_TGT, 
-                                      struct ioctl_arg);
-    lainmemu_ioctl_release_tgt = _IOR((char) dev_major, LAINMEMU_IOCTL_RELESE_TGT,
-                                      struct ioctl_arg);
-    lainmemu_ioctl_get_map     = _IOW((char) dev_major, LAINMEMU_IOCTL_GET_MAP,
-                                      struct ioctl_arg);
-    lainmemu_ioctl_get_map_sz  = _IOW((char) dev_major, LAINMEMU_IOCTL_GET_MAP_SZ,
-                                      struct ioctl_arg);
-    return 0;
-}
-
-
 //create an attribute in lainmemu to expose major number
-static long register_major_attr() {
+static long memu_register_major_attr(void) {
 
-    long ret;
+    int ret;
 
-    memu_major_attr = __ATTR_RO(memu_major);
-
-    ret = (long) sysfs_create_group(&lainmemu_device.kobj, &memu_attr_group);
+    ret = device_create_file(lainmemu_device, &lainmemu_major_attr);
     if (unlikely(ret)) {
-        device_destroy(lainmemu_device, MKDEV(dev_major, 0));
-        class_destroy(lain_class);
-        unregister_chrdev(dev_major, MEMU_DEVICE_NAME);
+        device_destroy(lainko_class, MKDEV(lainmemu_major, 0));
+        class_destroy(lainko_class);
+        unregister_chrdev(lainmemu_major, MEMU_DEVICE_NAME);
         printk(KERN_ALERT 
           "[lainmemu][ERR] failed to expose the major number attribute of lainmemu.\n");
         return PTR_ERR(lainmemu_device);
     }
 
-    printk(KERN_INFO "[lainmemu][OK] exposed major number as an attribute of lainmemu");
+    printk(KERN_INFO 
+      "[lainmemu][OK] exposed major number as an attribute of lainko sysfs class.\n");
 
     return 0;
 }
 
 
+//fill lainmemu ioctl numbers
+static long memu_fill_ioctl_nums(void) {
 
-static long (*init_fn[])()
-                = {register_major, register_class, register_device,
-                   fill_lainmemu_ioctl_nums, register_major_attr};
+    //TODO check if _IOWR is necessary when doing copy_to/from_user()
+    lainmemu_ioctl_open_tgt    = _IOW((char) lainmemu_major, LAINMEMU_IOCTL_OPEN_TGT, 
+                                      struct ioctl_arg);
+    lainmemu_ioctl_release_tgt = _IOW((char) lainmemu_major, LAINMEMU_IOCTL_RELEASE_TGT,
+                                      struct ioctl_arg);
+    lainmemu_ioctl_get_map     = _IOWR((char) lainmemu_major, LAINMEMU_IOCTL_GET_MAP,
+                                       struct ioctl_arg);
+    lainmemu_ioctl_get_map_sz  = _IOW((char) lainmemu_major, LAINMEMU_IOCTL_GET_MAP_SZ,
+                                      struct ioctl_arg);
+    return 0;
+}
+
+
+static long (*init_fn[])(void)
+                = {memu_register_major, register_class, memu_register_device,
+                   memu_register_major_attr, memu_fill_ioctl_nums};
 #define INIT_FN_LEN (sizeof(init_fn) / sizeof(init_fn[0]))
 
 
 // --- LOAD & UNLOAD
 
 //on lkm load
-static int __init lain_init(void) {
+static int __attribute__((unused)) __init lainko_init(void) {
 
     long ret;
 
@@ -257,12 +242,12 @@ static int __init lain_init(void) {
 
 
 //on lkm unload
-static int __fini lain_fini(void) {
+static int __attribute__((unused)) __exit lainko_exit(void) {
     
-    device_destroy(lainmemu_device, MKDEV(dev_major, 0));
-    class_unregister(lain_class);
-    class_destroy(lain_class);
-    unregister_chrdev(dev_major, MEMU_DEVICE_NAME);
+    device_destroy(lainko_class, MKDEV(lainmemu_major, 0));
+    class_unregister(lainko_class);
+    class_destroy(lainko_class);
+    unregister_chrdev(lainmemu_major, MEMU_DEVICE_NAME);
     printk(KERN_INFO "[lain][OK] module successfully unloaded.\n");
 
     return 0;
@@ -277,19 +262,19 @@ static int get_ioctl_arg(struct ioctl_arg * arg, struct ioctl_arg __user * arg_u
     unsigned long r_bytes;
 
     //check the passed userspace ptr is valid
-    if (!access_ok(arg_uptr, sizeof(ioctl_arg))) return -EFAULT;
+    if (!access_ok(arg_uptr, sizeof(struct ioctl_arg))) return -EFAULT;
 
     //get argument struct from userspace
     r_bytes = copy_from_user((void *) arg, 
                              (const void __user *) arg_uptr, sizeof(struct ioctl_arg));
         
-    if (rd_wr != 0) return -EFAULT;
+    if (r_bytes != 0) return -EFAULT;
     return 0;
 }
 
 
 //find task of pid & increment its refcount
-static struct task_struct * task_by_pid(pid_t pid) {
+static struct task_struct * task_by_pid(struct local_data * l_data_ptr, pid_t pid) {
 
     struct task_struct * iter;
 
@@ -310,12 +295,11 @@ static struct task_struct * task_by_pid(pid_t pid) {
 
 
 //get memory of a task & increment its refcount
-static struct mm_struct * get_mm(task_struct * task) {
+static struct mm_struct * get_mm_struct(struct task_struct * task) {
 
     struct mm_struct * mm;
     
-    mm = mm_access(task, PTRACE_MODE_ATTACH | PTRACE_MODE_FSCREDS
-                         | PTRACE_MODE_READ | PTRACE_MODE_WRITE);
+    mm = mm_access(task, PTRACE_MODE_ATTACH | PTRACE_MODE_FSCREDS | PTRACE_MODE_READ);
     if (IS_ERR_OR_NULL(mm)) {
 
         if (mm == NULL) return ERR_PTR(-EFAULT);
@@ -351,7 +335,7 @@ static void release_task_mm(struct local_data * l_data_ptr) {
 
 //check structure validity & acquire map operation locks
 static int prepare_map_operations(struct local_data * l_data_ptr,
-                                  struct vma_iterator vma_iter) {
+                                  struct vma_iterator * vma_iter) {
 
     //check target is set
     if (l_data_ptr->target_task == NULL) return -ESRCH;
@@ -397,10 +381,10 @@ static int build_vm_entry(struct vm_entry * vm_ent, struct vm_area_struct * vma)
 
 
     //build vm_entry
-    vm_ent.vm_start = vma->vm_start;
-    vm_ent.vm_end   = vma->vm_end;
-    vm_ent.file_off = vma->vm_pgoff;
-    vm_ent.prog     = vma->vm_page_prot;
+    vm_ent->vm_start = vma->vm_start;
+    vm_ent->vm_end   = vma->vm_end;
+    vm_ent->file_off = vma->vm_pgoff;
+    vm_ent->prot     = vma->vm_page_prot.pgprot;
    
     //if no backing file, make path empty
     if (vma->vm_file == NULL) {
@@ -408,12 +392,12 @@ static int build_vm_entry(struct vm_entry * vm_ent, struct vm_area_struct * vma)
     
     //otherwise get path string & set path buffer
     } else {
-        path_str = d_path(vma->vm_file->f_path, path_buf, PATH_MAX);
+        path_str = d_path(&vma->vm_file->f_path, path_buf, PATH_MAX);
         if (IS_ERR_OR_NULL(path_str)) {
-            return (int) path_str;
+            return (int) PTR_ERR(path_str);
         }
 
-        strncpy(vm_ent.file_path, path_str, PATH_MAX);
+        strncpy(vm_ent->file_path, path_str, PATH_MAX);
     } //end if
 
     return 0;
@@ -432,15 +416,15 @@ static int memu_open_tgt(struct local_data * l_data_ptr, struct ioctl_arg * arg)
     release_task_mm(l_data_ptr);
    
     //open new target
-    task_ret = task_by_pid(arg->target_pid);
+    task_ret = task_by_pid(l_data_ptr, arg->target_pid);
     if (IS_ERR(task_ret)) {
-        return (int) task_ret;
+        return (int) PTR_ERR(task_ret);
     }
 
     //become user of target's virtual memory
-    mm_ret = get_mm(task_ret);
+    mm_ret = get_mm_struct(task_ret);
     if (IS_ERR(mm_ret)) {
-        return (int) mm_ret;
+        return (int) PTR_ERR(mm_ret);
     }
 
     return 0;
@@ -473,7 +457,7 @@ static int memu_get_map(struct local_data * l_data_ptr, struct ioctl_arg * arg) 
 
     //allocate buffer for the map
     l_data_ptr->tx_buf = __vmalloc(l_data_ptr->tx_buf_sz, 
-                                   GFP_KERNEL | GFP_RETRY_MAYFAIL);
+                                   GFP_KERNEL | __GFP_RETRY_MAYFAIL);
     if (l_data_ptr->tx_buf == NULL) return -ENOMEM;
 
     //acquire locks & vma iterator
@@ -482,7 +466,7 @@ static int memu_get_map(struct local_data * l_data_ptr, struct ioctl_arg * arg) 
 
     
     //while there are VMAs left and there is space in the buffer
-    while ( (vma = vma_next(&vma_iter) != NULL) && (count < l_data_ptr->vm_map_sz) ) {
+    while ( ((vma = vma_next(&vma_iter)) != NULL) && (count < l_data_ptr->vm_map_sz) ) {
 
         //build vm_entry
         ret = build_vm_entry(&vm_ent, vma);
@@ -552,7 +536,7 @@ static int memu_get_map_sz(struct local_data * l_data_ptr) {
 
 
     //while there are VMAs left in this address space
-    while (vma = vma_next(&vma_iter)) {
+    while ((vma = vma_next(&vma_iter)) != NULL) {
         
         ++count;
         if (count < 0) {
@@ -588,18 +572,18 @@ static int memu_get_map_sz(struct local_data * l_data_ptr) {
 
 //read & write function (adapted from /fs/proc/base.c:837 mem_rw() v6.6.30)
 static ssize_t mem_rw(struct mm_struct * mm, char __user * u_buf, 
-                      size_t count, loff_t * ppos, bool write) {
+                      size_t count, loff_t * off_ptr, bool write) {
 
-    lain_byte * page;
+    lainko_byte * page;
 
     unsigned int flags = FOLL_FORCE | (write == MEMU_WRITE ? FOLL_WRITE : 0);
-    unsigned long remote_addr = *ppos;
+    unsigned long remote_addr = *off_ptr;
     ssize_t copied = 0;
 
     if (!mm) return 0;
 
     //get a temp buffer page
-    page = (lain_byte *) __get_free_page(GFP_KERNEL);
+    page = (lainko_byte *) __get_free_page(GFP_KERNEL);
     if (!page) return -ENOMEM;
 
     //increment mm's refcount unless it is already 0
@@ -621,17 +605,17 @@ static ssize_t mem_rw(struct mm_struct * mm, char __user * u_buf,
         }
 
         //perform the read/write operation into another vma space
-        this_len = access_remote_vm(mm, addr, remote_addr, page, this_len, flags);
+        this_len = access_remote_vm(mm, remote_addr, page, this_len, flags);
         if (!this_len) {
             if (!copied) copied = -EIO;
         }
 
-        if (!write && copy_to_user(buf, page, this_len)) {
+        if (!write && copy_to_user(u_buf, page, this_len)) {
             copied = -EFAULT;
             break;
         }
 
-        buf += this_len;
+        u_buf += this_len;
         remote_addr += this_len;
         copied += this_len;
         count -= this_len;
@@ -639,15 +623,14 @@ static ssize_t mem_rw(struct mm_struct * mm, char __user * u_buf,
     } //end while
 
     //update file position
-    *ppos = addr;
+    *off_ptr = remote_addr;
 
     //decrease refcount of mm
     mmput(mm);
 
 free:
 
-    free_page((unsigned long) page);
-    
+    free_page((unsigned long) page); 
     return copied;
 }
 
@@ -660,10 +643,10 @@ static loff_t memu_llseek(struct file * file_ptr, loff_t off, int whence) {
     switch (whence) {
 
         case SEEK_SET:
-            file->f_pos = off;
+            file_ptr->f_pos = off;
             break;
         case SEEK_CUR:
-            file->f_pos += off;
+            file_ptr->f_pos += off;
             break;
         default:
             return -EINVAL;
@@ -671,7 +654,7 @@ static loff_t memu_llseek(struct file * file_ptr, loff_t off, int whence) {
     } //end switch
 
     force_successful_syscall_return();
-    return file->f_pos;
+    return file_ptr->f_pos;
 }
 
 
@@ -701,7 +684,7 @@ static int memu_release(struct inode * inode_ptr, struct file * file_ptr) {
     release_task_mm(l_data_ptr);
 
     //free private data
-    kfree(file->private_data);
+    kfree(file_ptr->private_data);
 
     return 0;
 }
@@ -726,7 +709,7 @@ static long memu_ioctl(struct file * file_ptr,
     switch(ioctl_index) {
 
         case LAINMEMU_IOCTL_OPEN_TGT:            
-            ret = memu_set_tgt(l_data_ptr, &arg);
+            ret = memu_open_tgt(l_data_ptr, &arg);
             break;
 
         case LAINMEMU_IOCTL_RELEASE_TGT:
@@ -762,8 +745,8 @@ static ssize_t memu_read(struct file * file_ptr,
 
     //carry out the read
     read_bytes = mem_rw(l_data_ptr->target_mm, 
-                        (char __user *) u_buf, count, ppos, MEMU_READ);
-    if(IS_ERR(read_bytes)) {
+                        (char __user *) u_buf, count, off_ptr, MEMU_READ);
+    if(read_bytes < 0) {
         return read_bytes;
     }
 
@@ -782,9 +765,9 @@ static ssize_t memu_write(struct file * file_ptr,
 
     //carry out the write
     write_bytes = mem_rw(l_data_ptr->target_mm, 
-                         (char __user *) u_buf, count, ppos, MEMU_WRITE); 
-    if (IS_ERR(read_bytes)) {
-        return read_bytes;
+                         (char __user *) u_buf, count, off_ptr, MEMU_WRITE); 
+    if (write_bytes < 0) {
+        return write_bytes;
     }
 
     return 0;
